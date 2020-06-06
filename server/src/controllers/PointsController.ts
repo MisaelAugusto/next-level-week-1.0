dae16800c5936586e07232bb68cc1ca13fed9b23
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2020-06-05 22:12:53 -0300

    Fifth Day - Image Upload and Data Validation

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,6 +17,13 @@ class PointsController {
             .distinct()
             .select('points.*');
 
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.15.5:3333/uploads/${point.image}`
+            };
+        })
+
         return response.json(points);
     }
 
@@ -29,12 +36,17 @@ class PointsController {
             return response.status(400).json({ message: 'Point not found.' });
         }
 
+        const serializedPoint = {
+            ...point,
+            image_url: `http://192.168.15.5:3333/uploads/${point.image}`
+        };
+
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title');
 
-        return response.json({ point, items });
+        return response.json({ point: serializedPoint, items });
     }
 
     async create(request: Request, response: Response) {
@@ -52,7 +64,7 @@ class PointsController {
         const trx = await knex.transaction();
     
         const point = {
-            image: 'https://images.unsplash.com/photo-1534723452862-4c874018d66d?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -65,7 +77,9 @@ class PointsController {
     
         const point_id = insertedIds[0];
     
-        const pointItems = items.map((item_id: Number) => {
+        const pointItems = items.split(', ')
+                                .map((item: string) => Number(item.trim()))
+                                .map((item_id: Number) => {
             return ({
                 item_id,
                 point_id
